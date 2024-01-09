cdfbccc8d1603f0eb9f418b4a0c3d60d58736234
Ninjaa <ahlulffirdaus@gmail.com>
2024-01-09 11:02:47 +0700

    a

--- app/Views/user/post.php
+++ app/Views/user/post.php
@@ -1,45 +1,56 @@
 <?= $this->extend('layouts/layoutUser'); ?>
 <?= $this->section('content'); ?>
 
-      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content rounded-0">
-          <div class="modal-body py-0">
-
-            
-            <div class="d-flex main-content">
-              <div class="bg-image promo-img mr-3" style="background-image: url('images/img_1.jpg');">
-                <span class="price">$2000</span>
-              </div>
-              <div class="content-text p-4 px-5 align-item-stretch">
-                <div class="text-center">
-                    <a href="#" class="share"><span class="icon-share"></span></a>
-                    <h3 class="mb-3 line">Grand Prix 4000</h3>
-                    <p class="mb-3">
-                        <span class="icon-star text-warning"></span>
-                        <span class="icon-star text-warning"></span>
-                        <span class="icon-star text-warning"></span>
-                        <span class="icon-star text-warning"></span>
-                        <span class="icon-star"></span>
-                    </p>
-
-                    <p class="mb-5">All their equipment and instruments are alive. The sky was this is cloudless and of a deep dark blue. A shining crescent far beneath the flying vessel.</p>
-
-                    <div class="d-flex text-center social w-50 mx-auto">
-                        <a href="#" class="d-inline-block d-flex align-items-center mr-auto like">
-                            <span class="icon-heart mr-2 icon"></span> <span>25</span>
-                        </a>
-                        <a href="#" class="d-inline-block d-flex align-items-center mx-auto message">
-                            <span class="icon-chat mr-2 icon"></span> <span>10</span>
-                        </a>
-                        <a href="#" class="d-inline-block d-flex align-items-center ml-auto add">
-                            <span class="icon-add mr-2"></span> <span>55</span>
-                        </a>
-                    </div>
-                </div>
-              </div>
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+      <div class="window">
+        <img class="foto" src="https://i.pinimg.com/736x/47/5e/ba/475eba57454015bfe214e8722e2422a1.jpg" alt="">
+        <div class="hover-zone" onclick="redirectToPage('/post')">
+          <div class="top-bar">
+            <a href="#" class="saveButton">Save</a>
+          </div>
+          <!-------------------->
+          <div class="bottom-bar">
+            <a href="#" class="webButton">↗ website.com</a>
+            <div class="radius-ico">
+              <a href="#" class="iconButton"><i class="fa-regular fa-heart fa-xl"></i></a>
+              <a href="#" class="iconButton"><i class="fa-solid fa-download fa-xl"></i></a>
             </div>
-
           </div>
         </div>
       </div>
+    </div>
+    <div class="col-8">
+      <!-- buatlah sebuah kolom komentar seperti aplikasi instagram -->
+      <div class="containerPost border-mix melengkung">
+        <div class="row">
+          <div class="col-12">
+            <div class="display-flex">
+          <a href="/profile"><img tabindex="1" draggable="false" class="iconpost pp" src="https://i.pinimg.com/564x/bd/94/ce/bd94ce28cf8aefb521bac31d547f6409.jpg"></a>
+          Hai
+          </div>
+            <h3 class="mt-3">Title</h3>
+            <p>Description</p>
+          </div>
+
+        </div>
+
+        <div class="row mt-5">
+          <div class="col-12">
+            <form action="/komentar/" method="post">
+              <?= csrf_field(); ?>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Komentar" name="komentar" id="komentar">
+                <div class="input-group-append">
+                  <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Kirim</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <?= $this->endSection(); ?>
