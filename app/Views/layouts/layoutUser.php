7d9e8d1853e27932e61c9fb6750df0d75f2d0de2
Ninjaa <ahlulffirdaus@gmail.com>
2024-03-03 22:01:28 +0700

    a

--- app/Views/layouts/layoutUser.php
+++ app/Views/layouts/layoutUser.php
@@ -23,7 +23,7 @@
 
   <meta charset="UTF-8">
   <meta name="description" content="Gallery Website">
-  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="keywords" content="HTML, CSS, PHP, JavaScript">
   <meta name="author" content="Ahlfss">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
